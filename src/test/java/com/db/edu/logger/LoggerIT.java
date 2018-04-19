3f331495bb7579956f84ad1c89e9035c07b512fb
elenabaykalova <elena.baykalova@hotmail.com>
2018-04-19 11:54:40 +0300

    Introduce simple app.

---
+++ src/test/java/com/db/edu/logger/LoggerIT.java
@@ -0,0 +1,8 @@
+package com.db.edu.logger;
+
+public class LoggerIT {
+    public static void main(String[] args) {
+        LoggerController logger = new LoggerController();
+        logger.log("test message");
+    }
+}
